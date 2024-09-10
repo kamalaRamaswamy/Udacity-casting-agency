f0d3e102f4bf4d836f2b39f432a5e4506816d713
Kamala <kamala.ramaswamyk@gmail.com>
2024-09-10 13:52:22 +0530

    Reverted change

--- app.py
+++ app.py
@@ -278,5 +278,8 @@ def create_app(test_config=None):
     return app
 
 app = create_app()
-app.run(host='0.0.0.0', port=8080, debug=True)
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
