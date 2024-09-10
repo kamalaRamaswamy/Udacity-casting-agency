017c94ae5fb6e458f711b8e21c5bed46bf0830be
Kamala <kamala.ramaswamyk@gmail.com>
2024-09-10 13:45:45 +0530

    port bind

--- app.py
+++ app.py
@@ -278,8 +278,5 @@ def create_app(test_config=None):
     return app
 
 app = create_app()
-
-if __name__ == '__main__':
-    app = create_app()
-    app.run(host='0.0.0.0', port=8080, debug=True)
+app.run(host='0.0.0.0', port=8080, debug=True)
 
