2c109c748a069aa76290677630b24bbddd4fc0da
Kamala <kamala.ramaswamyk@gmail.com>
2024-09-11 11:45:08 +0530

    Unit tests and Readme updates

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-Readme file - to be updated
+DESCRIPTION
+
+The Casting Agency models a company that is responsible for creating movies and managing and assigning actors to those movies. You are an Executive Producer within the company and are creating a system to simplify and streamline your process.
+
+Hosted on Render - https://udacity-casting-agency-dfc1.onrender.com/actors
+
+
+ROLES
+
+Casting assistant 
+casting directors 
+executive directors
+
+
+***********************************************************************************************************
+
+## Setting up the Backend
+
+### Install Dependencies
+
+1. **Python 3.7** - Follow instructions to install the latest version of python for your platform in the [python docs](https://docs.python.org/3/using/unix.html#getting-and-installing-the-latest-version-of-python)
+
+2. **Virtual Environment** - We recommend working within a virtual environment whenever using Python for projects. This keeps your dependencies for each project separate and organized. Instructions for setting up a virual environment for your platform can be found in the [python docs](https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/)
+
+3. **PIP Dependencies** - Once your virtual environment is setup and running, install the required dependencies by navigating to the `/backend` directory and running:
+
+```bash
+pip install -r requirements.txt
+```
+
+To run the server, execute:
+
+```bash
+flask run --reload
+```
+
+***********************************************************************************************************
+
+
+
+***********************************************************************************************************
+
+API Server
+All accessable endpoints of the project are located in the app.py file.
+
+Run the following command in the project root folder to start the local development server:
+
+flask run
+
+***********************************************************************************************************
+
+API endpoints
+
+Public endpoints
+
+GET '/movies'
+Fetches a dictionary with id and title of movies who posted their release date to the database.
+Request Arguments: None
+Returns: A JSON object with two keys: 'success' and 'success meessage' - a dictionary with company id and name.
+Sample curl request: curl -X GET http://127.0.0.1:5000/movies 
+
+Sample response:
+
+{
+    "movies": {
+        "1": "Kyle XY",
+        "2": "Black Panther",
+        "3": "Bob"
+    },
+    "success": true
+}
+
+Sample curl request: curl -X GET http://127.0.0.1:5000/companies 
+
+
---
+++ test_app.py
@@ -0,0 +1,88 @@
+import os
+from app import create_app
+from models import setup_db, Movie, Actor
+import unittest
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+database_username = os.getenv("database_username")
+database_password = os.getenv("database_password")
+database_name = os.getenv("database_name")
+
+
+test_actor = Actor(
+    name='aaa',
+    age='32',
+    gender='female')
+
+test_movie = Movie(
+    title='Home Alone',
+    release_data='2000-03-08')
+
+class CapstoneTest(unittest.TestCase):
+    def setUp(self):
+
+        DB_PATH = os.getenv("database_username")
+        praducer_token = os.getenv("PRODUCER_TOKEN")
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.praducer_token = praducer_token
+        self.test_actor_data = {"name": "Vanessa", "age": 32, "gender":"female"}
+        self.test_movie_data = {"title": "Mission Impossible", "release_data":"2018-08-31"}
+
+
+    def tearDown(self):
+        """Executed after each test"""
+        pass
+
+
+    def test_get_actors(self):
+        response = self.client().get('/actors',
+            headers={'Authorization': f'Bearer {self.praducer_token}'})
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_movies(self):
+        response = self.client().get('/movies',
+            headers={'Authorization': f'Bearer {self.praducer_token}'})
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_post_actor(self):
+        response = self.client().post('/actors',
+            json=self.test_actor_data,
+            headers={'Authorization': f'Bearer {self.praducer_token}'})
+
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['name'], 'Vanessa')
+        self.assertEqual(data['age'], 32)
+        self.assertEqual(data['gender'], 'female')
+
+    def test_post_movie(self):
+        response = self.client().post('/movies',
+            json=self.test_movie_data,
+            headers={'Authorization': f'Bearer {self.praducer_token}'})
+
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['title'], 'Mission Impossible')
+        self.assertEqual(data['release_data'],'2018-08-31')
+
+    
+    def test_patch_actor(self):
+        new_actor = {"name": "Lily", "age": 19, "gender": "female"}
+        response = self.client().patch('/actors/1',
+            json=new_actor,
+            headers={'Authorization': f'Bearer {self.praducer_token}'})
+
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
